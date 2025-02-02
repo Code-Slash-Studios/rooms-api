1a6ff0ec1eff058274c87d3999697a609bc20bc3
ncaldwell02 <nathan.d.caldwell@gmail.com>
2025-02-02 16:40:15 -0500

    endpoints to test

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -9,14 +9,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	var res models.Reservation
-	var response http.Response
-	var example string
-
-	fmt.Printf("Test")
-}
-
 var reservations = make(map[string]models.Reservation)
 
 func getReservations(w http.ResponseWriter, r *http.Request) {
@@ -36,4 +28,13 @@ func getReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	json.NewEncoder(w).Encode(reservation)
+}
+
+func main() {
+	api := mux.NewRouter()
+	api.handlefunc("/reservations", getReservations).Methods("GET")
+	api.handlefunc("/reservations/{id}", getReservation).Methods("GET")
+
+	fmt.Printf("API listening on port 6000")
+	http.ListenAndServe(":6000", api)
 }
