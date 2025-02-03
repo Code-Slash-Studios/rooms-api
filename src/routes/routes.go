af2e4fcbe784102945e0bc1246043c16f46bf36f
ncaldwell02 <nathan.d.caldwell@gmail.com>
2025-02-03 10:14:35 -0500

    more endpoints

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -30,10 +30,44 @@ func getReservation(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(reservation)
 }
 
+func createReservation(w http.ResponseWriter, r *http.Request) {
+	var reservation models.Reservation
+	json.NewDecoder(r.Body).Decode(&reservation)
+	reservations[reservation.ID] = reservation
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(reservation)
+}
+
+func updateReservation(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var updatedReservation models.Reservation
+	json.NewDecoder(r.Body).Decode(&updatedReservation)
+	if _, exists := reservations[params["id"]]; !exists {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+	updatedReservation.ID = params["id"]
+	reservations[params["id"]] = updatedReservation
+	json.NewEncoder(w).Encode(updatedReservation)
+}
+
+func deleteReservation(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	if _, exists := reservations[params["id"]]; !exists {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+	delete(reservations, params["id"])
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
 	api := mux.NewRouter()
 	api.handlefunc("/reservations", getReservations).Methods("GET")
 	api.handlefunc("/reservations/{id}", getReservation).Methods("GET")
+	api.HandleFunc("/users", createReservation).Methods("POST")
+	api.HandleFunc("/users/{id}", updateReservation).Methods("PUT")
+	api.HandleFunc("/users/{id}", deleteReservation).Methods("DELETE")
 
 	fmt.Printf("API listening on port 6000")
 	http.ListenAndServe(":6000", api)
